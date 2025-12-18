313b4dfe8a8d4561004e71311b7bfda62b8ff313
Arnaud Lier <arnaud@lier.link>
2025-12-18 18:00:45 +0100

    print clock from CMOS

--- main.c
+++ main.c
@@ -228,6 +228,60 @@ __attribute__((interrupt)) void keyboard_handler(struct interrupt_stack_frame *i
 		.offset_2 = ((unsigned int) &handler) >> 16,\
 	}
 
+static inline unsigned int bcd(unsigned char a) {
+	return ( (a & 0xF0) >> 1) + ( (a & 0xF0) >> 3) + (a & 0xf);
+}
+
+enum cmos_register {
+	RTC_SECOND = 0x00,
+	RTC_MINUTE = 0x02,
+	RTC_HOUR = 0x04,
+	RTC_DAY = 0x07,
+	RTC_MONTH = 0x08,
+	RTC_YEAR = 0x09,
+	RTC_USUAL_CENTURY = 0x32 // not the same on every machine?
+};
+
+unsigned char from_cmos(enum cmos_register reg) {
+	// does not handle binary format
+	// is it a good idea to disable NMI?
+	outb(PORT_CMOS_INDEX, reg);
+	return bcd(inb(PORT_CMOS_DATA));
+}
+
+// print clock from CMOS
+void print_clock(void) {
+	char buf[11];
+
+	unsigned int century = from_cmos(RTC_USUAL_CENTURY);
+	unsigned int year = from_cmos(RTC_YEAR);
+	unsigned int month = from_cmos(RTC_MONTH);
+	unsigned int day = from_cmos(RTC_DAY);
+	unsigned int hour = from_cmos(RTC_HOUR);
+	unsigned int minute = from_cmos(RTC_MINUTE);
+	unsigned int second = from_cmos(RTC_SECOND);
+
+	write("date is ");
+	uint32_str_10(buf, hour);
+	write(buf);
+	write(":");
+	uint32_str_10(buf, minute);
+	write(buf);
+	write(":");
+	uint32_str_10(buf, second);
+	write(buf);
+	write(" ");
+	uint32_str_10(buf, day);
+	write(buf);
+	write("/");
+	uint32_str_10(buf, month);
+	write(buf);
+	write("/");
+	uint32_str_10(buf, century * 100 + year);
+	write(buf);
+	write("\n");
+}
+
 void kernel_main(struct multiboot_info *multi) {
 	char buf[11];
 	write("multiboot size: ");
@@ -243,6 +297,8 @@ void kernel_main(struct multiboot_info *multi) {
 		flag++;
 		flag = (unsigned int *)((unsigned char *)flag + *flag + 4);
 	}
+	write("\n");
+	print_clock();
 
 	idt[INT_BREAKPOINT] = DEF_INTERRUPT(breakpoint_handler);
 	idt[INT_DOUBLE_FAULT] = DEF_INTERRUPT(double_fault_handler);

0f803b96762f22e8a19e51ad9ccbd7b506662dcb
Arnaud Lier <arnaud@lier.link>
2025-12-19 15:22:03 +0100

    add simple date command

--- shell.c
+++ shell.c
@@ -1,14 +1,32 @@
 #include "libk/libk.h"
 #include "vga/vga.h"
 #include "io.h"
+#include "common.h"
+
+void print_clock(void);
 
 void handle_command(unsigned char len, const char *cmd) {
-	if (len == 4 && memcmp(cmd, "stop", 4) == 0) {
-		writes("stopping...\n");
-		// TODO: proper shutdown
-		outw(0x604, 0x2000); // QEMU shutdown only
+	bool found = true;
+
+	switch (len) {
+	case 4:
+		if (memcmp(cmd, "stop", 4) == 0) {
+			writes("stopping...\n");
+			// TODO: proper shutdown
+			outw(0x604, 0x2000); // QEMU shutdown only
+		} else if (memcmp(cmd, "help", 4) == 0) {
+			writes("help:\n"
+				"- stop: power off the machine\n"
+				"- date: print RTC/wall-clock date and time\n");
+		} else if (memcmp(cmd, "date", 4) == 0) {
+			print_clock();
+		} else
+			found = false;
+		break;
+	default:
+		found = false;
 	}
-	else if (len)
+	if (!found && len)
 		writes("command not found!\n");
 
 	writes("> ");

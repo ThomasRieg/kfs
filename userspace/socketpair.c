02b86ac26f3a37a875e00a294e7c5fa4127b6fce
Thomas Rieg <thrieg@student.42mulhouse.fr>
2026-03-24 14:44:20 +0100

    added socketpair error handling'

--- userspace/socketpair.c
+++ userspace/socketpair.c
@@ -2,15 +2,23 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
 	int sv[2];
 	int socketpairr = socketpair(AF_UNIX, SOCK_STREAM, 0, sv);
 	printf("socketpair returned %d + two sockets %d and %d\n", socketpairr, sv[0], sv[1]);
+	if (socketpairr < 0)
+		perror("socketpair");
 
 	char buf[6] = "hello";
 	int writer = write(sv[0], buf, 6);
 	printf("write returned %d\n", writer);
+	if (writer < 0)
+		perror("writer");
 
-	int readr = read(sv[1], buf, 6);
-	printf("read returned %d: %s\n", readr, buf);
+	char read_buf[6];
+	int readr = read(sv[1], read_buf, 6);
+	printf("read returned %d: %s\n", readr, read_buf);
+	if (readr < 0)
+		perror("readr");
 }
